cbbe8031f4838af79b3da165a59d20b2ded4ae15
Pratham90123 <achauhanprathamz84@gmail.com>
2025-08-05 13:15:40 +0530

    made calculator

--- index.java
+++ index.java
@@ -3,20 +3,36 @@ import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    
-     System.out.print("Enter the first number: ");
+
+        // Take first number
+        System.out.print("Enter the first number: ");
         int num1 = scanner.nextInt();
 
-        // Ask for the second number
+        // Take second number
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
 
-         scanner.close();  
+        // Take operator
+        System.out.println("Choose one operator: add, sub, mul, div");
+        String op = scanner.next();  // Fixed: removed pattern
 
+        // Perform operation
+        if (op.equals("add")) {
+            System.out.println("Result: " + (num1 + num2));
+        } else if (op.equals("sub")) {
+            System.out.println("Result: " + (num1 - num2));
+        } else if (op.equals("mul")) {
+            System.out.println("Result: " + (num1 * num2));
+        } else if (op.equals("div")) {
+            if (num2 != 0) {
+                System.out.println("Result: " + (num1 / num2));
+            } else {
+                System.out.println("Cannot divide by zero.");
+            }
+        } else {
+            System.out.println("Invalid operator.");
+        }
 
-
-
+        scanner.close();
     }
-
-    
 }
